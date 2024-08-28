1f1c379e51851253ffb8a6571261035aba0eca52
Trương Minh Nhật <fleeforezz@gmail.com>
2024-08-28 11:26:05 +0700

    Update

--- Ansible/run-script.sh
+++ Ansible/run-script.sh
@@ -3,7 +3,7 @@
 # Print out the job is starting
 echo 'Starting'
 
-ansible-playbook playbooks.yml --user nhat --ask-pass --ask-become-pass -i hosts
+ansible-playbook playbooks.yml --user nhat -i hosts
 
 # Print out if the job is complete
 echo 'Run complete !!!'
